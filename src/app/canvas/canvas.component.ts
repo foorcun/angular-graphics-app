4e7ab2b1c9cc9dcf1e79e6b20095eadeb7c2c4ca
foorcun <msnifoorcun@hotmail.com>
2024-06-29 13:54:56 +0300

    global frame - rectangle rotation is successfull

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { count } from 'console';
+import { VectorObject2D } from './canvas.model';
+
 
 @Component({
   selector: 'app-canvas',
@@ -21,20 +22,20 @@ export class CanvasComponent implements AfterViewInit {
 
 
 
-    // if (this.ctx) {
-    //   this.ctx.fillStyle = 'white';
-    //   this.ctx.fillRect(0, 0, width, height);
+    if (this.ctx) {
+      //   this.ctx.fillStyle = 'white';
+      //   this.ctx.fillRect(0, 0, width, height);
+      //   this.ctx.fillStyle = 'black';
+      //   this.ctx.fillRect(width / 2, height / 2, 20, 20);
+      // this.rectangleWith4Parameter();
+    }
 
-    //   this.ctx.fillStyle = 'black';
-    //   this.ctx.fillRect(width / 2, height / 2, 20, 20);
-    // }
   }
 
   putRectangle() {
     if (this.ctx) {
       // this.ctx.fillStyle = 'white';
       // this.ctx.fillRect(0, 0, this.width, this.height);
-
       this.ctx.fillStyle = 'red';
       this.ctx.fillRect(this.width / 2, this.height / 2, 20, 20);
 
@@ -49,7 +50,7 @@ export class CanvasComponent implements AfterViewInit {
     if (this.ctx) {
       this.ctx.fillStyle = 'black';
       // this.ctx.fillRect(10, 10, 5, 5);
-      this.ctx.fillRect(this.width/2, this.height/2, 5, 5);
+      this.ctx.fillRect(this.width / 2, this.height / 2, 5, 5);
     }
   }
   drawLine() {
@@ -76,37 +77,134 @@ export class CanvasComponent implements AfterViewInit {
     }
   }
 
-  frameTransformation(){
+  frameTransformation() {
     var vectorGlobal = [50, 50];
 
-    var vector = [vectorGlobal[0] ,-vectorGlobal[1]]; // transformation
+    var vector = [vectorGlobal[0], -vectorGlobal[1]]; // transformation
     console.log(vector);
-    vector = [vector[0] + this.width/2, vector[1] + this.height/2]; // oteleme
+    vector = [vector[0] + this.width / 2, vector[1] + this.height / 2]; // oteleme
     console.log(vector);
 
     if (this.ctx) {
       this.ctx.beginPath();
-      this.ctx.moveTo(this.width/2, this.height/2);
+      this.ctx.moveTo(this.width / 2, this.height / 2);
       this.ctx.lineTo(vector[0], vector[1]);
       this.ctx.stroke();
     }
   }
 
-  rotateLineGlobal(){
+  rotateLineGlobal() {
     var vectorGlobal = [50, 50];
-    var vector = [vectorGlobal[0] ,-vectorGlobal[1]]; // transformation
-    vector = this.multiplyMatrixVector(this.rotationMatrix(this.rotateCounter * 15), vector); 
-    vector = [vector[0] + this.width/2, vector[1] + this.height/2]; // oteleme
+    var vector = [vectorGlobal[0], -vectorGlobal[1]]; // transformation
+    vector = this.multiplyMatrixVector(this.rotationMatrix(this.rotateCounter * 15), vector);
+    vector = [vector[0] + this.width / 2, vector[1] + this.height / 2]; // oteleme
     console.log(vector);
-    
+
     this.rotateCounter++;
 
     if (this.ctx) {
       this.ctx.beginPath();
-      this.ctx.moveTo(this.width/2, this.height/2);
+      this.ctx.moveTo(this.width / 2, this.height / 2);
       this.ctx.lineTo(vector[0], vector[1]);
       this.ctx.stroke();
     }
+  }
+
+  changeFrame(newBasisMatrix: number[], coordinate: VectorObject2D) {
+    var inverseBasisMatrix = [[1, 0], [0, -1]];
+
+    // for (let i = 0; i < coordinates.length; i++) {
+    var coordinateNew = this.multiplyMatrixVectorObject(inverseBasisMatrix, coordinate);
+    // console.log(coordinateNew)
+    // console.log(coordinates[i].x)
+    coordinate.x = coordinateNew[0] + 400; // +400 oteleme icin.
+
+    // console.log(coordinates[i].x)
+    coordinate.y = coordinateNew[1] + 300;
+    // 
+    // console.log(coordinates)
+    return coordinate;
+  }
+  rotatePoint(coordinate: VectorObject2D, angle: number){
+    var rotationMatrix = this.rotationMatrix(angle);
+    var coordinateNew = this.multiplyMatrixVectorObject(rotationMatrix, coordinate);
+    return new VectorObject2D(coordinateNew[0],coordinateNew[1])
+  }
+
+  rectangleRotate(coordinates: VectorObject2D[]) {
+    
+    this.clear();
+    
+    for (let i = 0; i < coordinates.length; i++) {
+
+      coordinates[i] = this.rotatePoint(coordinates[i],15 * this.rotateCounter)
+
+      coordinates[i] = this.changeFrame([], coordinates[i])
+    }
+    
+    this.rectangleWith4Coordinates(coordinates);
+
+    this.rotateCounter++;
+  }
+
+  rectangleWith4Coordinates(coordinates: any) {
+
+    // Begin path
+    this.ctx.beginPath();
+
+    // Move to the first corner
+    this.ctx.moveTo(coordinates[0].x, coordinates[0].y);
+
+    // Draw lines to each of the subsequent corners
+    for (let i = 1; i < coordinates.length; i++) {
+      this.ctx.lineTo(coordinates[i].x, coordinates[i].y);
+    }
+
+    // Close the path by connecting the last corner to the first
+    this.ctx.closePath();
+
+    // Set fill color
+    this.ctx.fillStyle = 'blue';
+
+    // Fill the rectangle
+    this.ctx.fill();
+
+    this.ctx.strokeStyle = 'red';
+    this.ctx.stroke();
+
+  }
+  rectangleWith4Parameter() {
+
+    // Corner coordinates
+    const corners = [
+      { x: 50, y: 70 },
+      { x: 350, y: 50 },
+      { x: 150, y: 150 },
+      { x: 50, y: 150 }
+    ];
+    // Begin path
+    this.ctx.beginPath();
+
+    // Move to the first corner
+    this.ctx.moveTo(corners[0].x, corners[0].y);
+
+    // Draw lines to each of the subsequent corners
+    for (let i = 1; i < corners.length; i++) {
+      this.ctx.lineTo(corners[i].x, corners[i].y);
+    }
+
+    // Close the path by connecting the last corner to the first
+    this.ctx.closePath();
+
+    // Set fill color
+    this.ctx.fillStyle = 'blue';
+
+    // Fill the rectangle
+    this.ctx.fill();
+
+    this.ctx.strokeStyle = 'red';
+    this.ctx.stroke();
+
   }
 
   clear() {
@@ -118,7 +216,7 @@ export class CanvasComponent implements AfterViewInit {
 
   rotationMatrix(angle: number) {
     return [
-      [Math.cos(this.angleToRadianConverter(angle)), -Math.sin(this.angleToRadianConverter(angle))],
+      [Math.cos(this.angleToRadianConverter(angle)), -1 * Math.sin(this.angleToRadianConverter(angle))],
       [Math.sin(this.angleToRadianConverter(angle)), Math.cos(this.angleToRadianConverter(angle))]
     ];
   }
@@ -128,14 +226,37 @@ export class CanvasComponent implements AfterViewInit {
     const x = vector[0];
     const y = vector[1];
 
+    // console.log(x)
+    // console.log(y)
     const result = [
       matrix[0][0] * x + matrix[0][1] * y,
       matrix[1][0] * x + matrix[1][1] * y
     ];
+    // console.log(matrix[0][0])
+    // console.log(result)
+    return result;
+  }
+
+  // Function to multiply a 2D rotation matrix by a column vector
+  multiplyMatrixVectorObject(matrix: number[][], vector: VectorObject2D) {
+    const x = vector.x;
+    const y = vector.y;
+
+    console.log(x);
+    console.log(y);
+    console.log(matrix);
+    const result = [
+      matrix[0][0] * x + matrix[0][1] * y,
+      matrix[1][0] * x + matrix[1][1] * y
+    ];
+    console.log(matrix[1][1]);
+    console.log(result);
 
     return result;
   }
+
   angleToRadianConverter(angle: number) {
     return Math.PI * (angle / 180);
   }
+
 }
---
+++ src/app/canvas/canvas.model.ts
@@ -0,0 +1,10 @@
+
+export class VectorObject2D {
+  x: number;
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+}
